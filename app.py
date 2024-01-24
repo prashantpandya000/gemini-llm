0a98d68176264c553dad3a1eef0240e7d98c3979
prashantpandya000 <prashantpandya46@gmail.com>
2024-01-25 00:55:57 +0530

    Incomplete Gemini code need to solve the audio reading challenge

--- app.py
+++ app.py
@@ -1,5 +1,20 @@
 import streamlit as st
 
+
+#necessary library
+import pathlib
+import textwrap
+
+import google.generativeai as genai
+
+from IPython.display import display
+from IPython.display import Markdown
+
+
+def to_markdown(text):
+  text = text.replace('•', '  *')
+  return Markdown(textwrap.indent(text, '> ', predicate=lambda _: True))
+
 def main():
     st.title("Google gemini app")
     input=st.text_input("Enter your question Here")
